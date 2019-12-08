ae52f47ccce806055ce7452f5794d959e6230a97
Summer Rizzo <srainrizzo@gmail.com>
2019-12-08 02:00:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,6 +9,7 @@ def get_japanese_emoticon (file, west_emo)
   emo_lib = load_library(file)
 
   japan_emo = emo_lib.find{|meaning, emoticons| emoticons[0] == west_emo, emoticons}[1]
+  puts japan_emo
 
 end
 
