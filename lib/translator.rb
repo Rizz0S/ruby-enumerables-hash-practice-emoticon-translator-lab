d367bb999cc10d94174c3ba49c05a838bd20371a
Summer Rizzo <srainrizzo@gmail.com>
2019-12-08 02:37:02 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,17 +13,21 @@ def load_library (file)
     h[:get_meaning][emoticons[1]] = meaning
     h[:get_emoticon][emoticons[0]] = emoticons[1]
   end
-
+  pp h
   h
 end
 
 def get_japanese_emoticon (file, west_emo)
   emo_lib = load_library(file)
 
-  japan_emo = emo_lib.find{|meaning, emoticons| emoticons[0] == west_emo}.last[1]
+
+
+  #japan_emo = emo_lib.find{|meaning, emoticons| emoticons[0] == west_emo}.last[1]
 
 end
 
 def get_english_meaning (japan_emo)
   # code goes here
 end
+
+load_library("./lib/emoticons.yml")
