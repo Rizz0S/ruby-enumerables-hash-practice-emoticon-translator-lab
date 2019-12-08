1b9bd7db8e3ab26a409488be41b9179d8c68a6b8
Summer Rizzo <srainrizzo@gmail.com>
2019-12-08 02:10:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,11 +10,6 @@ def get_japanese_emoticon (file, west_emo)
 
   japan_emo = emo_lib.find{|meaning, emoticons| emoticons[0] == west_emo}.last[1]
 
-  if !(japan_emo)
-    return "Sorry, that emoticon was not found"
-  end
-  japan_emo
-
 end
 
 def get_english_meaning (japan_emo)
