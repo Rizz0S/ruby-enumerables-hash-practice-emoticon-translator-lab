bda567d8d1897d65a83d288e2f97974e6f945200
Summer Rizzo <srainrizzo@gmail.com>
2019-12-08 01:53:17 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,8 +5,10 @@ def load_library (file)
   YAML.load_file(file)
 end
 
-def get_japanese_emoticon (west_emo)
+def get_japanese_emoticon (file, west_emo)
+  emo_lib = load_library(file)
 
+  japan_emo = emo_lib.each_value {|value|}
 
 end
 
