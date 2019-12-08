b79c360d24971c014e9fbda872c68929667046a8
Summer Rizzo <srainrizzo@gmail.com>
2019-12-08 02:36:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,5 +1,6 @@
 # require modules here
 require "yaml"
+require "pp"
 
 def load_library (file)
   emo_lib = YAML.load_file(file)
@@ -19,12 +20,10 @@ end
 def get_japanese_emoticon (file, west_emo)
   emo_lib = load_library(file)
 
-#  japan_emo = emo_lib.find{|meaning, emoticons| emoticons[0] == west_emo}.last[1]
+  japan_emo = emo_lib.find{|meaning, emoticons| emoticons[0] == west_emo}.last[1]
 
 end
 
 def get_english_meaning (japan_emo)
   # code goes here
 end
-
-get_japanese_emoticon("./lib/emoticons.yml", ":)")
